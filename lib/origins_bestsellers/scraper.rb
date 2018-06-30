a75ee19691b7615c4064ffa0cc2ad647b93f5bcb
Ariel Zhen <zhenpingping@live.com>
2018-06-30 08:36:05 +0000

    scraper methods

--- lib/origins_bestsellers/scraper.rb
+++ lib/origins_bestsellers/scraper.rb
@@ -1,3 +1,15 @@
 class Scraper
 
+  def self.scrape_skincare_page
+    skincare_page = Nokogiri::HTML(open("https://www.origins.com/skincare-bestsellers"))
+  end
+
+  def self.scrape_bath_and_body_page
+    bath_and_body_page = Nokogiri::HTML(open("https://www.origins.com/bathandbody-bestsellers"))
+  end
+
+  def self.scrape_makeup_page
+    makeup_page = Nokogiri::HTML(open("https://www.origins.com/makeup-bestsellers"))
+  end
+
 end
