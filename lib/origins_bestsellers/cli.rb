64ffa79c245a7063eb34c855b88de74857e7e247
Ariel Zhen <zhenpingping@live.com>
2018-06-30 19:32:17 +0000

    fix syntax error

--- lib/origins_bestsellers/cli.rb
+++ lib/origins_bestsellers/cli.rb
@@ -5,7 +5,7 @@ class OriginsBestsellers::CLI
     puts "Please view the bestsellers of Origins from 3 catagories: 1.SKINCARE, 2.BATH & BODY, 3.MAKEUP"
     puts "Which catagory would you like to see the list of best sellers? Please type in the corresponding number for each catagory or type exit:"
     input = gets.strip
-    
+
   end
 
 
@@ -16,10 +16,13 @@ class OriginsBestsellers::CLI
   end
 
 
-  def display_products(@product_array)
+  def display_products
     @product_array.each.with_index(1) do |product, index|
       puts "#{index} - #{product.name} - #{product.price}"
       puts "-----------"
+    end
   end
 
 end
+
+# ./bin/origins-bestsellers
