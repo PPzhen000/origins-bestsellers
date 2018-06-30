1c3c399036cdbc9a78f17c6163eda99a5a9983fc
Ariel Zhen <zhenpingping@live.com>
2018-06-30 08:45:30 +0000

    product class

--- lib/origins_bestsellers/product.rb
+++ lib/origins_bestsellers/product.rb
@@ -1,3 +1,21 @@
 class Product
+  attr_accessor :name, :price
+  @@all = []
 
+  def initialize(pruduct_hash)
+    product_hash.each do |attribute, value|
+      self.send(("#{attribute}=") ,value)
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create_from_product_array(product_array) #return value of Scraper.scrape_page methods
+    product_array.each do |each_product|
+      Product.new(each_product)
+    end
+  end
 end
