208323b953752dba1c2f4246f8e568f38232e65b
Ariel Zhen <zhenpingping@live.com>
2018-06-30 08:37:16 +0000

    scrape skincare_page

--- lib/origins_bestsellers/scraper.rb
+++ lib/origins_bestsellers/scraper.rb
@@ -2,6 +2,15 @@ class Scraper
 
   def self.scrape_skincare_page
     skincare_page = Nokogiri::HTML(open("https://www.origins.com/skincare-bestsellers"))
+    skincare_product = []
+    count = 0
+    while skincare_page.css(".product-brief__subtitle").size > count do
+      product_name = skincare_page.css(".product-brief__subtitle")[count].text.strip
+      product_price = skincare_page.css(".product-brief__price.down-price")[count].text.strip
+      skincare_product[count] = {name: product_name, price: product_price}
+      count +=1
+    end
+    skincare_product
   end
 
   def self.scrape_bath_and_body_page
