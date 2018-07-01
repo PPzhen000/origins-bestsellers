3e44ebe7d7dab827c0ccec1c3cc783ffcee04b39
Ariel Zhen <zhenpingping@live.com>
2018-07-01 05:30:45 +0000

    version 0.1.0

--- lib/origins_bestsellers/cli.rb
+++ lib/origins_bestsellers/cli.rb
@@ -1,26 +1,29 @@
 class OriginsBestsellers::CLI
 
   def call
+    input = ""
     puts "Welcome! You are viewing Origins bestsellers!"
+    while input != "exit"
     puts "Please view the bestsellers of Origins from 3 catagories: 1.SKINCARE, 2.BATH & BODY, 3.MAKEUP"
-    puts "Which catagory would you like to see the list of best sellers? Please type in the corresponding number for each catagory or type exit:"
+    puts "Which catagory would you like to see the list of best sellers? Please type in the corresponding number for each catagory"
+    puts "To quit, type 'exit'."
     input = gets.strip
 
-    @product_array = []
-   case input
-   when "1"
-     @product_array = OriginsBestsellers::Product.skincare_product
-     display_products
-   when "2"
-     @product_array = OriginsBestsellers::Product.bath_and_body_product
-     display_products
-   when "3"
-     @product_array = OriginsBestsellers::Product.makeup_product
-     display_products
-   when "exit"
-     goodbye
-   end
-
+      @product_array = []
+      case input
+      when "1"
+        @product_array = OriginsBestsellers::Product.skincare_product
+        display_products
+      when "2"
+        @product_array = OriginsBestsellers::Product.bath_and_body_product
+        display_products
+      when "3"
+        @product_array = OriginsBestsellers::Product.makeup_product
+        display_products
+      when "exit"
+        goodbye
+      end
+    end
   end
 
   def goodbye
