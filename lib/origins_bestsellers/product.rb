6b245b08c91e98ce65a39156575a076f19371403
Ariel Zhen <zhenpingping@live.com>
2018-07-17 07:00:53 +0000

    refactoring

--- lib/origins_bestsellers/product.rb
+++ lib/origins_bestsellers/product.rb
@@ -1,34 +1,10 @@
 class OriginsBestsellers::Product
   attr_accessor :name, :price, :url
-  @@all = []
 
   def initialize(name = nil, price = nil, url = nil)
     @name = name
     @price = price
     @url = url
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.create_from_product_array(product_array) #return value of Scraper.scrape_page methods
-    product_array.each do |each_product|
-      OriginsBestsellers::Product.new(each_product)
-    end
-  end
-
-  def self.skincare_product
-    self.create_from_product_array(OriginsBestsellers::Scraper.scrape_skincare_page)
-    binding.pry
-  end
-
-  def self.bath_and_body_product
-    self.create_from_product_array(OriginsBestsellers::Scraper.scrape_bath_and_body_page)
-  end
-
-  def self.makeup_product
-    self.create_from_product_array(OriginsBestsellers::Scraper.scrape_makeup_page)
   end
 
 end
