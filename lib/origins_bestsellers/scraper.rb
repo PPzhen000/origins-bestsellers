897adc4b08b1126f86226386c50ad77f260e1887
Ariel Zhen <zhenpingping@live.com>
2018-07-17 06:59:30 +0000

    arrays of objs

--- lib/origins_bestsellers/scraper.rb
+++ lib/origins_bestsellers/scraper.rb
@@ -2,21 +2,21 @@ class OriginsBestsellers::Scraper
 
   def self.scrape_page(url)
     doc = Nokogiri::HTML(open(url))
-    product = []
+    product_obj = []
     doc.css(".product-grid-wrapper").each do |products|
       products.css(".js-product.js-product-pr.product-brief").each do |each_product|
-        product_name = each_product.css("a.product-brief__subtitle__link.js-spp-link").text
-        product_price = each_product.css(".product-brief__price.down-price").text.strip
-        product_url = "https://www.origins.com" + "#{each_product.css("h3").css("a").attr("href").text}"
-        product << {name: product_name, price: product_price, url: product_url}
+        product = OriginsBestsellers::Product.new
+        product.name = each_product.css("a.product-brief__subtitle__link.js-spp-link").text
+        product.price = each_product.css(".product-brief__price.down-price").text.strip
+        product.url = "https://www.origins.com" + "#{each_product.css("h3").css("a").attr("href").text}"
+        product_obj << product
       end
     end
-      product
+      product_obj
   end
 
   def self.scrape_skincare_page
     self.scrape_page("https://www.origins.com/skincare-bestsellers")
-    # binding.pry
   end
 
   def self.scrape_bath_and_body_page
